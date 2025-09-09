13f6319914b10668ca52363ee9b2bd2eb1472532
Stefani <stefanimonteiroribeiro@hotmail.com>
2025-09-09 17:09:25 -0300

    Tudo pronto, mensagens de erro ok e tudo funcionando! <3

--- src/lista_compras.py
+++ src/lista_compras.py
@@ -1,15 +1,19 @@
 lista_compras=[]
 escolha_menu=1
 
-print("Bem-vindo à sua lista de compras!")
+print("\nBem-vindo à sua lista de compras!\n")
 
 while escolha_menu!=5:
-    print("1-Adicionar itens à minha lista")
+    print("\n1-Adicionar itens à minha lista")
     print("2-Editar um item da minha lista")
     print("3-Excluir um item da minha lista")
     print("4-Revisar a minha lista")
-    print("5-Finalizar minha lista")
-    escolha_menu=int(input("Escolha a opção desejada: "))  
+    print("5-Finalizar minha lista\n")
+    try:
+        escolha_menu=int(input("Escolha a opção desejada: "))
+    except ValueError:
+        print("\nErro! Por favor, digite apenas um número.\n")
+        continue
     
     #Adicionar itens na lista
     if escolha_menu==1:        
@@ -20,73 +24,100 @@ while escolha_menu!=5:
             else:
                 lista_compras.append(item_atual)
                 lista_formatada=", ".join(lista_compras)
-        print("A sua lista de compras atual é:",lista_formatada)
+        print("\nA sua lista de compras atual é:",lista_formatada)
         print(("\nDeseja adicionar mais um item à lista?\n1-Sim\n2-Não, voltar ao menu"))
-        escolha=int(input("Escolha a opção desejada: "))
+        try:
+            escolha=int(input("\nEscolha a opção desejada: "))
+        except ValueError:
+            print("\nErro! Por favor, digite apenas um número.")
+            continue
         while escolha==1:
-            lista_compras.append(input("Digite o item a ser adicionado: "))
+            lista_compras.append(input("Digite o item a ser adicionado: \n"))
             lista_formatada=", ".join(lista_compras)
+            print("\nItem adicionado com sucesso!\n")
             print("Sua lista atual tem:",lista_formatada)
-            print("Deseja adicionar mais um item?\n1-Sim\n2-Não\n")
-            escolha=int(input("Escolha a opção desejada: "))
+            print("\nDeseja adicionar mais um item?\n1-Sim\n2-Não")
+            try:
+                escolha=int(input("\nEscolha a opção desejada: "))
+            except ValueError:                
+                print("\nErro! Por favor, digite apenas um número.")
+                continue
     #Editar itens na lista
     elif escolha_menu==2:
         quer_editar=1
         if len(lista_compras)==0:
-            print("Não há itens para editar!")
-            print("Voltando ao menu:")
+            print("\nNão há itens para editar!")
+            print("\nVoltando ao menu...\n")
             escolha_menu=1
         elif len(lista_compras)>=1:
             while quer_editar==1:
-                print("Qual item você gostaria de editar?")
+                print("\nQual item você gostaria de editar?")
                 for posicao, item in enumerate(lista_compras):
                     print(posicao+1,item)
-                escolha_do_usuario=int(input("Digite o número do item: "))
+                try:
+                    escolha_do_usuario=int(input("\nDigite o número do item: "))
+                except ValueError:
+                    print("\nErro! Por favor, digite apenas um número.")
+                    continue
                 indice_real=escolha_do_usuario-1
                 lista_compras[indice_real]=input("Digite o nome do item para editá-lo: ")
                 lista_formatada=", ".join(lista_compras)
-                print("Produto editado com sucesso!")
+                print("\nProduto editado com sucesso!\n")
                 print("A sua lista de compras atual é:",lista_formatada)
-                print("Deseja editar outro item?\n1-Sim\n2-Não, voltar ao menu")
-                quer_editar=int(input("Escolha a opção desejada: "))
+                print("\nDeseja editar outro item?\n1-Sim\n2-Não, voltar ao menu")
+                try:
+                    quer_editar=int(input("\nEscolha a opção desejada: "))
+                except ValueError:
+                    print("\nErro! Por favor, digite apenas um número.")
+
     #Exclui itens na lista
     elif escolha_menu==3:
         quer_excluir=1
-        if len(lista_compras)==0:
-            print("Não há itens para excluir!")
-            print("Voltando ao menu:")
-            escolha_menu=2
-        elif len(lista_compras)>=1:
-            print("Qual item você deseja excluir?")
-            for posicao, item in enumerate(lista_compras):
-                print(posicao+1,item)
-            escolha_do_usuario=int(input("Digite o número do item:"))
-            indice_real=escolha_do_usuario-1
-            lista_compras.pop(indice_real)
-            lista_formatada=", ".join(lista_compras)
-            print("Produto excluído com sucesso!")
-            if len(lista_compras)>=1:                    
-                print("A sua lista de compras atual é:",lista_formatada)
-                print("Deseja excluir outro item?\n1-Sim\n2-Não, voltar ao menu")
-                quer_excluir=int(input("Escolha a opção desejada: "))
-            else:
-                print("A sua lista de compras atual está vazia!")
-                print("Não há mais itens para excluir!")
-                print("Voltando ao menu...")
+        while quer_excluir==1:
+            if len(lista_compras)==0:
+                print("\nNão há itens para excluir!\n")
+                print("Voltando ao menu...\n")
                 escolha_menu=2
+            elif len(lista_compras)>=1:
+                print("\nQual item você deseja excluir?\n")
+                for posicao, item in enumerate(lista_compras):
+                    print(posicao+1,item)
+                try:
+                    escolha_do_usuario=int(input("\nDigite o número do item:"))
+                except ValueError:                    
+                    print("\nErro! Por favor, digite apenas um número.")
+                    continue
+                indice_real=escolha_do_usuario-1
+                lista_compras.pop(indice_real)
+                lista_formatada=", ".join(lista_compras)
+                print("\nProduto excluído com sucesso!\n")
+                if len(lista_compras)>=1:                    
+                    print("A sua lista de compras atual é:",lista_formatada)
+                    print("\nDeseja excluir outro item?\n1-Sim\n2-Não, voltar ao menu")
+                try:
+                    quer_excluir=int(input("\nEscolha a opção desejada: "))
+                except ValueError:
+                    print("\nErro! Por favor, digite apenas um número.")
+                    continue
+                else:
+                    print("\nA sua lista de compras atual está vazia!")
+                    print("Não há mais itens para excluir!")
+                    print("\nVoltando ao menu...\n")
+                    escolha_menu=2
+
     #Revisar a lista
     elif escolha_menu==4:
-        print("A sua lista contém:")
+        print("\nA sua lista contém:")
         for posicao, item in enumerate(lista_compras):
                 print(item)
 
 lista_formatada=", ".join(lista_compras)
     
-print("A sua lista está pronta!")
+print("\nA sua lista está pronta!\n")
 for posicao, item in enumerate(lista_compras):
     n_itens=len(lista_compras)
     print(item)
 print("Você precisa comprar",n_itens,"itens!")
-print("Boas compras!")
+print("\nBoas compras!\n")
 
 
