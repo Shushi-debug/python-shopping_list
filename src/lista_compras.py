62ed4d543c211a3c609a7eceb66061e461cb5e58
Stefani <stefanimonteiroribeiro@hotmail.com>
2025-09-09 16:27:43 -0300

    Tudo funcional, revisar lista adicionado e mensagem de finalização melhorada

--- src/lista_compras.py
+++ src/lista_compras.py
@@ -3,21 +3,23 @@ escolha_menu=1
 
 print("Bem-vindo à sua lista de compras!")
 
-while escolha_menu!=4:
+while escolha_menu!=5:
     print("1-Adicionar itens à minha lista")
     print("2-Editar um item da minha lista")
     print("3-Excluir um item da minha lista")
-    print("4-Finalizar minha lista")
+    print("4-Revisar a minha lista")
+    print("5-Finalizar minha lista")
     escolha_menu=int(input("Escolha a opção desejada: "))  
     
     #Adicionar itens na lista
-    if escolha_menu==1:
-        n_itens=int(input("Digite quantos itens você deseja colocar na lista de compras: "))
-        count=0
-        for i in range(n_itens):
-            lista_compras.append(input("Digite o item para adicionar à lista de compras: "))
-            lista_formatada=", ".join(lista_compras)
-
+    if escolha_menu==1:        
+        while True:
+            item_atual=input("Digite um item e dê enter (ou digite 'fim' para terminar):")
+            if item_atual=="fim" or item_atual=="Fim":
+                break
+            else:
+                lista_compras.append(item_atual)
+                lista_formatada=", ".join(lista_compras)
         print("A sua lista de compras atual é:",lista_formatada)
         print(("\nDeseja adicionar mais um item à lista?\n1-Sim\n2-Não, voltar ao menu"))
         escolha=int(input("Escolha a opção desejada: "))
@@ -72,12 +74,19 @@ while escolha_menu!=4:
                 print("Não há mais itens para excluir!")
                 print("Voltando ao menu...")
                 escolha_menu=2
+    #Revisar a lista
+    elif escolha_menu==4:
+        print("A sua lista contém:")
+        for posicao, item in enumerate(lista_compras):
+                print(item)
 
 lista_formatada=", ".join(lista_compras)
     
-print("A sua lista está pronta, você precisa comprar:")
-for item in lista_compras:
+print("A sua lista está pronta!")
+for posicao, item in enumerate(lista_compras):
+    n_itens=len(lista_compras)
     print(item)
+print("Você precisa comprar",n_itens,"itens!")
 print("Boas compras!")
 
 
